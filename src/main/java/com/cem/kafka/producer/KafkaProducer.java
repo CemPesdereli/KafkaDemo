0c156b50fa0d45b8759a1c67f1b7fc49b766aaea
CemPesdereli <cempesdereli@gmail.com>
2024-12-02 18:00:30 +0300

    init project

---
+++ src/main/java/com/cem/kafka/producer/KafkaProducer.java
@@ -0,0 +1,26 @@
+package com.cem.kafka.producer;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import static java.lang.String.*;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String msg){
+        log.info(format("Sending message to cem Topic:: %s",msg));
+        kafkaTemplate.send("cem",msg);
+    }
+
+
+
+
+}
